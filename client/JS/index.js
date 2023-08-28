a254d0030b0b4ab3d2d92ff3b867ccfb9925135b
CybLiz <liza.bahloul@gmail.com>
2023-08-28 09:18:08 +0200

    linker toutes les page, css regle modifié, buttons

---
+++ client/JS/index.js
@@ -0,0 +1,8 @@
+let startButton = document.querySelector('.start-button');
+
+startButton.addEventListener('click', () => {
+	// Rediriger l'utilisateur vers une nouvelle page HTML
+	window.location.href = './regle.html';
+
+});
+
